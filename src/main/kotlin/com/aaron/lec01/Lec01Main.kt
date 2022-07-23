ea0126a8d2f10bc1da1a0dd678e2eab9fdebbff1
aaronsleepy <aaron@kmong.com>
2022-07-23 21:35:30 +0900

    lec 1

---
+++ src/main/kotlin/com/aaron/lec01/Lec01Main.kt
@@ -0,0 +1,16 @@
+package com.aaron.lec01
+
+fun main() {
+    var number1 = 10L
+    val number2 = 10L
+
+    // Nullable
+    var number3: Long? = 1_000L
+    number3 = null
+
+    var person = Person("aaron")
+}
+
+class Person(name: String) {
+
+}
